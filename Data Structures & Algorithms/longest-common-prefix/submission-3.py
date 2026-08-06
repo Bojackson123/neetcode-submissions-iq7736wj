bcbccc6a4b8315613366947b6ed66b301da6f70e
Rashid Al-Marri <98349907+Bojackson123@users.noreply.github.com>
2026-08-05 21:26:28 -0500

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,11 @@
+from sortedcontainers import SortedDict
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort(key=len)
+        
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if strs[0][i] != strs[j][i]:
+                    return strs[0][:i]
+        return strs[0]
